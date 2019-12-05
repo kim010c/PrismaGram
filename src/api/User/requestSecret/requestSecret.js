6584e207de53026b8ea3893e43a29662da48f89a
Park Jongho <kim010c@gmail.com>
2019-12-06 02:56:15 +0900

    # BugPix
    RequestSecret console bugpix

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,14 +4,14 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args) => {
-      console.log(request.user);
       const { email } = args;
       const loginSecret = generateSecret();
       try {
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch {
+      } catch (error) {
+        console.log(error);
         return false;
       }
     }
