7ce4f9473b11497eb0ea1164b49eecca2e13b8e6
Park Jongho <kim010c@gmail.com>
2019-12-06 18:55:02 +0900

    #4-1 createAccount schema 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,6 +3,10 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("해당 username은 이미 사용중입니다.");
+      }
       await prisma.createUser({
         username,
         email,
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -10,8 +10,8 @@ export default {
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
-        console.log(error);
+      } catch (e) {
+        console.log(e);
         return false;
       }
     }
